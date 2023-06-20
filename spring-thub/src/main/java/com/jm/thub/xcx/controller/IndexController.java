9ad906ac46a09bc0c182e84bbb89b4db7190fa38
caozhenhao <905017337@qq.com>
2023-06-20 21:30:13 +0800

    首页接口

---
+++ spring-thub/src/main/java/com/jm/thub/xcx/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.jm.thub.xcx.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author caozhenhao
+ * @version 1.0
+ * @date 2023/6/20 21:29
+ */
+@RestController
+public class IndexController {
+
+}
---
+++ spring-thub/src/main/java/com/jm/thub/xcx/controller/MsgController.java
@@ -0,0 +1,10 @@
+package com.jm.thub.xcx.controller;
+
+/**
+ * @author caozhenhao
+ * @version 1.0
+ * @date 2023/6/20 21:28
+ */
+public class MsgController {
+
+}
